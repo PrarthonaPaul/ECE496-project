46d4e4a0e9f9b32e62175820e87894e73508e8ab
Anipreet-Chowdhury <anipreet.c@gmail.com>
2024-08-19 15:47:57 +0530

    Adding in a view to take care of registration and login, also creating a basic template for a login page and a registration page

--- webapp/djangobackend/file_processor/urls.py
+++ webapp/djangobackend/file_processor/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
+from django.urls import path  #path is used to define url patterns
+from django.contrib.auth import views as auth_views # django's inbuilt authentication module which has views like Login-view
+from . import views #imports views from file_processor
 
 urlpatterns = [
-    path ('login/', auth_views.LoginView.as_view(template_name = 'login.html'),name='login'),
-    path('register/',views.register, name='register')
+    path ('login/', auth_views.LoginView.as_view(template_name = 'login.html'),name='login'), #sets up the login url, using the standard LoginView template to serve the login page
+    path('register/',views.register, name='register') #sets up the registration url
 ]
 
--- webapp/djangobackend/file_processor/views.py
+++ webapp/djangobackend/file_processor/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-# Create your views here.
+
+#Creating a view for the registration process
+def register(request):
+    #if block is for users who are already registered. This checks password and redirects to user-landing page
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('user_landing')
+    #else block is for first time registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
