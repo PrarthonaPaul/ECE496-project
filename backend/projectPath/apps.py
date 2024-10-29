42da639c7a975a1324cadca2c063bc1c4ca48a52
Prarthona Paul <p.paul@mail.utoronto.ca>
2024-10-29 15:06:28 -0400

    added backend to accept pdf files

---
+++ backend/projectPath/apps.py
@@ -0,0 +1,6 @@
+# projectPath/apps.py
+from django.apps import AppConfig
+
+class ProjectPathConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'projectPath'  # This should match the name you added to INSTALLED_APPS
